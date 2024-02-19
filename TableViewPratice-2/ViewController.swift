dec919a8bad7e2f9792424b16ab622f38f327e9c
Howe <howewu1991@gmail.com>
2024-02-19 22:07:06 +0800

    Initial Commit

---
+++ TableViewPratice-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPratice-2
+//
+//  Created by Howe on 2024/2/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
